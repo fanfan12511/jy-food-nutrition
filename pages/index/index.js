00a2a57eb8d2e2fc22ed3ea5efcdb9531e83004d
fanfan12511 <sunfan@joyoung.com>
2018-07-09 08:55:21 +0800

    食谱小样

---
+++ pages/index/index.js
@@ -0,0 +1,102 @@
+//index.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    list:[{
+        id:'01',
+        title:'蔬菜热量排名',
+        url:'',
+        img:'../../images/img01.jpeg'
+      },{
+        id: '02',
+        title: '高钙食物排名',
+        url: '',
+        img: '../../images/img05.jpeg'
+      }, {
+        id: '03',
+        title: '坚果、种子排名',
+        url: '',
+        img: '../../images/img03.jpeg'
+      },{
+        id: '04',
+        title: '肉、蛋、奶热量排名',
+        url: '',
+        img: '../../images/img04.jpeg'
+      },{
+        id: '05',
+        title: '水果热量排名',
+        url: '',
+        img: '../../images/img02.jpeg'
+      },{
+        id:'06',
+        title:'高GI食物排名',
+        url:'',
+        img:'../../images/img01.jpeg'
+      },{
+        id: '07',
+        title: '高嘌呤食物排名',
+        url: '',
+        img: '../../images/img01.jpeg'
+      }
+    ]
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+    
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+    
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+    
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+    
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+    
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+    
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+    
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+    
+  }
+})
---
+++ pages/index/pages/search/search.js
@@ -0,0 +1,77 @@
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    list:[
+      {
+        id:'hot',
+        name:'热门搜索',
+        tags:['苹果','香蕉','馒头','红薯','酸奶','豆浆']
+
+      }, {
+        id: 'history',
+        name: '历史搜索',
+        tags: ['南瓜','玉米','土豆','西红柿','牛肉']
+
+      }
+    ]
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+    
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+    
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+    
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+    
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+    
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+    
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+    
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+    
+  }
+})
