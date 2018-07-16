39930fd0367b4d8ebe0805a4a4613e5064daeb86
fanfan12511 <sunfan@joyoung.com>
2018-07-16 10:15:47 +0800

    搜索，排行榜

---
+++ pages/index/pages/leaderboard/leaderboard.js
@@ -0,0 +1,92 @@
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    items: [
+      {
+        id: '西瓜',
+        name: '西瓜',
+        num: '93',
+        unit: 'g/100g',
+        left: '0',
+        img: '../../../../images/img01.jpeg',
+        url: ''
+      },
+      {
+        id: '西红柿',
+        name: '西红柿',
+        num: '93',
+        unit: 'GI值:',
+        left: '1',
+        img: '../../../../images/img01.jpeg',
+        url: ''
+      }, {
+        id: '西红柿',
+        name: '西红柿',
+        num: '93',
+        unit: 'GI值:',
+        left: '1',
+        img: '../../../../images/img01.jpeg',
+        url: ''
+      },
+    ]
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+    
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+    
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+    
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+    
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+    
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+    
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+    
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+    
+  }
+})
